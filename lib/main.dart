36074a005a07046c5796c1c0f981e0dbf2e348e8
Team Web <wteam5274@gmail.com>
2020-01-22 15:40:03 +0100

    TextWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(
-      Container(
+  runApp(
+    Container(
         color: Colors.red,
-      ),
-    );
+        child: Text("Hello From Flutter",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 32.0, color: Colors.white))),
+  );
 }
