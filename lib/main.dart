e908093a64d30d8bd7d9be4ee49579c9fd8f437f
Team Web <wteam5274@gmail.com>
2020-01-23 11:14:35 +0100

    Stateful widget state

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,32 @@ class MyStatefulWidgets extends StatefulWidget {
 }
 
 class _MyStatefulWidgetsState extends State<MyStatefulWidgets> {
+  var myColor = Colors.red;
+
+  changeColor() {
+    setState(() {
+      print(myColor);
+      if (myColor == Colors.blue) {
+        myColor = Colors.red;
+      } else {
+        myColor = Colors.blue;
+      }
+
+      print(myColor);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container(
-        color: Colors.red,
+        color: myColor,
         child: Center(
           child: RaisedButton(
             child: Text("click", textDirection: TextDirection.ltr),
             onPressed: () {
               print("Hi from MyStatefulWidget");
+              changeColor();
             },
           ),
         ),
