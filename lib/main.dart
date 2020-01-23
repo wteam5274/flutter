367dc937f2cd1e6941dfd171cf684353de54e450
Team Web <wteam5274@gmail.com>
2020-01-23 14:18:07 +0100

    Material Design App bar : with icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyStatefulWidgets());
+  runApp(MaterialApp(
+    home: MyStatefulWidgets()
+  ));
 }
 
 class MyStatefulWidgets extends StatefulWidget {
@@ -12,57 +14,34 @@ class MyStatefulWidgets extends StatefulWidget {
 class _MyStatefulWidgetsState extends State<MyStatefulWidgets> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 40.0, right: 8.0),
-      child: Column(
-        children: <Widget>[
-          Card(
-              child: Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Row(
-              textDirection: TextDirection.ltr,
-              children: <Widget>[
-                Text(
-                  "Child 1",
-                  textDirection: TextDirection.ltr,
-                ),
-                Text(
-                  "Child 2",
-                  textDirection: TextDirection.ltr,
-                ),
-                Text(
-                  "Child 3",
-                  textDirection: TextDirection.ltr,
-                ),
-              ],
-            ),
-          )),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Card(
-                child: Padding(
-              padding: EdgeInsets.all(24.0),
-              child: Row(
-                textDirection: TextDirection.ltr,
-                children: <Widget>[
-                  Text(
-                    "Child 1",
-                    textDirection: TextDirection.ltr,
-                  ),
-                  Text(
-                    "Child 2",
-                    textDirection: TextDirection.ltr,
-                  ),
-                  Text(
-                    "Child 3",
-                    textDirection: TextDirection.ltr,
-                  ),
-                ],
-              ),
-            )),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Beginner",textDirection: TextDirection.ltr,),
+        actions: <Widget>[
+          IconButton(
+            icon: IconButton(icon: Icon(Icons.beenhere), onPressed: () {
+              print("icon beenhere");
+            }),
+          ),
+          IconButton(
+            icon: IconButton(icon: Icon(Icons.map), onPressed: () {
+              print("icon map");
+            }),
           )
         ],
       ),
     );
+
   }
+
+
+
+
+
+
+
+
+
+
+
 }
