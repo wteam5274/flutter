71aca6e65c7bfef3b76b057245619d45cb65fb52
Team Web <wteam5274@gmail.com>
2020-01-24 15:02:47 +0100

    Listview Items

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
-import 'screen2.dart';
-import 'screen3.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyStatefulWidgets(),
     theme: ThemeData(
-      primaryColor: Colors.red,
-      accentColor: Colors.lime,
-      brightness: Brightness.dark
-    ),
-    routes: <String, WidgetBuilder>{
-      "/screen2": (BuildContext context) => Screen2(),
-      "/screen3": (BuildContext context) => Screen3(),
-    },
+        primaryColor: Colors.red,
+        accentColor: Colors.lime,
+        brightness: Brightness.dark),
   ));
 }
 
@@ -24,34 +17,34 @@ class MyStatefulWidgets extends StatefulWidget {
 }
 
 class _MyStatefulWidgetsState extends State<MyStatefulWidgets> {
-  bool checkInput = false;
-  int gender = 1;
+  List list = List();
+
+  @override
+  void initState() {
+    super.initState();
+    list.add("nr.1");
+    list.add("nr.2");
+    list.add("nr.3");
+    list.add("nr.4");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Routing"),
-        actions: <Widget>[
-          IconButton(
-            icon: IconButton(icon: Icon(Icons.arrow_forward), onPressed: () {
-              print("icon arrow_forward");
-              Navigator.pushNamed(context, '/screen2');
-            }),
-          ),
-        ],
-      ),
-    );
-
+        appBar: AppBar(
+          title: Text("ToDos"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        body: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Dismissible(
+                key: Key(index.toString()),
+                child: ListTile(title: Text(list[index])),
+              );
+            }));
   }
-
-
-
-
-
-
-
-
-
-
-
 }
